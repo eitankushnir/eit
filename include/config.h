fb1648db6768ebf52cbec7230b58d494d559bd24
eitankushnir <etn121415@gmail.com>
2026-02-27 17:38:26 +0200

    feat. config reading

--- include/config.h
+++ include/config.h
@@ -3,12 +3,14 @@
 
 #define CONFIG_FILE ".eitconfig"
 
+
 enum scope {
     GLOBAL,
     LOCAL
 };
 
 char* read_config_str(char* category, char* key);
+// 1 - true, 0 - false, -1 - no such key. -2 - not a boolean.
 int read_config_bool(char* category, char* key);
 int read_config_int(char* category, char* key);
 
--- src/config.c
+++ src/config.c
@@ -2,6 +2,7 @@
 #include "strbuf.h"
 #include "wrappers.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void add_config(enum scope scope, char* category, char* key, char* value)
@@ -56,5 +57,102 @@ void add_config(enum scope scope, char* category, char* key, char* value)
         fclose(conffile);
     } else {
         writeline(keyvalue.buf, last_line_read, path.buf);
+    }
+
+    strbuf_free(&keyvalue);
+    strbuf_free(&category_header);
+    strbuf_free(&lines);
+}
+
+static char* find_value(char* path, char* category, char* key)
+{
+    int found_cat_flag = 0;
+    strbuf category_header = STRBUF_INIT;
+    strbuf lines = STRBUF_INIT;
+    strbuf value = STRBUF_INIT;
+    strbuf_addf(&category_header, "[%s]\n", category);
+
+    FILE* conffile = fopen(path, "rb");
+    if (conffile) {
+        while (strbuf_read_file_line(&lines, conffile) == 0) {
+            if (found_cat_flag && lines.buf[0] == '[') {
+                break;
+            }
+
+            if (found_cat_flag) {
+                char* delim = " = ";
+                char* token = strtok(lines.buf, delim);
+                while (token != NULL) {
+                    // ignore the leading tab.
+                    if (strcmp(token + 1, key) == 0) {
+                        strbuf_free(&value);
+                        strbuf_init(&value);
+                        token = strtok(NULL, delim);
+                        strbuf_addstr(&value, token);
+                    }
+                    token = NULL;
+                }
+            }
+
+            if (strcmp(lines.buf, category_header.buf) == 0) {
+                found_cat_flag = 1;
+            }
+
+            // Reached the category after.
+
+            strbuf_free(&lines);
+            strbuf_init(&lines);
+        }
+        fclose(conffile);
+    }
+    strbuf_free(&lines);
+    strbuf_free(&category_header);
+    if (value.len == 0) {
+        strbuf_free(&value);
+        return NULL;
+    }
+
+    char* value_str = strbuf_detach(&value, 0);
+    value_str[strlen(value_str) - 1] = '\0'; // remove trailing newline.
+    return value_str;
+}
+
+char* read_config_str(char* category, char* key)
+{
+    // 0. first try local - if not found try global.
+    // 1. find the cat
+    // 2. read the entire cat
+    // 3. return the last value set for the key.
+
+    strbuf path = STRBUF_INIT;
+    strbuf_addf(&path, "./%s", CONFIG_FILE);
+
+    char* value = find_value(path.buf, category, key);
+    if (value) {
+        return value;
+    }
+
+    strbuf_free(&path);
+    strbuf_init(&path);
+    strbuf_addf(&path, "~/%s", CONFIG_FILE);
+
+    value = find_value(path.buf, category, key);
+    return value;
+}
+
+int read_config_bool(char* category, char* key)
+{
+    char* raw = read_config_str(category, key);
+    if (!raw)
+        return -1;
+
+    if (strcmp(raw, "true") == 0) {
+        free(raw);
+        return 1;
+    } else if (strcmp(raw, "false") == 0) {
+        free(raw);
+        return 0;
+    } else {
+        return -2;
     }
 }

16ec409d166efe6dc8584ca33ad0ee0151761d85
eitankushnir <etn121415@gmail.com>
2026-03-09 17:09:40 +0200

    fixes

--- src/commands/write-tree.c
+++ src/commands/write-tree.c
@@ -3,19 +3,21 @@
 #include "stage.h"
 #include "tree.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int cmd_write_tree(char **argv, int argc, repository *repo) {
 
-    if (!stage_can_be_written(repo)) {
+    if (!stage_can_be_written(repo->stage)) {
         fprintf(stderr, "Error: The stage is not in a fully merged state. Cannot write a tree");
         return 1;
     }
 
     tree_node tree;
-    object_id* oid;
-    construct_stage_tree(&tree, repo);
+    construct_stage_tree(&tree, repo->stage);
     write_tree(&tree, repo);
-    printf("%s\n", tree.hex_oid);
+    char* hex_str = oid_tostring(&tree.oid);
+    printf("%s\n", hex_str);
     free_tree(&tree);
+    free(hex_str);
     return 0;
 }

107ac02748e11b38dee6a553cc87bd37cd51d997
eitankushnir <etn121415@gmail.com>
2026-05-04 17:44:29 +0300

    added object store to repo and a getter acces method

--- include/repository.h
+++ include/repository.h
@@ -1,11 +1,14 @@
 #ifndef REPOSITORY_H
 #define REPOSITORY_H
 
+#include "object_store.h"
 #define REPO_DIR_NAME ".eit"
 
 struct repository {
   char *repodir;  // Absolute path to the .eit directory.
   char *worktree; // Absolute path to the root of the project.
+
+  struct object_store *objects;
 };
 
 void repository_init(struct repository *repo);
@@ -13,5 +16,7 @@ void repository_release(struct repository *repo);
 
 char *repo_find_repo_dir(void);
 char *repo_find_repo_worktree(void);
+
+struct object_store *repo_get_object_store(struct repository *repo);
 
 #endif

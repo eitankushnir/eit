d89727123641914872b9c6d5186a5136bc95292d
eitankushnir <etn121415@gmail.com>
2026-05-04 20:30:40 +0300

    hashtable to store objects that have been brought into memory

--- include/object.h
+++ include/object.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include "sha256.h"
+#include <stddef.h>
 enum object_type {
   OBJ_UNKNOWN = 0, // Usually means some sort of failure to find or identify.
   OBJ_BLOB = 1,
@@ -8,9 +10,35 @@ enum object_type {
   OBJ_COMMIT = 3,
 };
 
+struct object {
+  struct object_id oid;
+  enum object_type type;
+};
+
+struct object_bucket {
+  struct object_bucket *next;
+  struct object *obj;
+};
+
+struct parsed_object_pool {
+  size_t bucket_nr;
+  struct object_bucket **buckets;
+};
+
 static const char *object_type_names[] = {"unknown", "blob", "tree", "commit"};
 
 const char *object_type_to_string(enum object_type type);
 enum object_type string_to_object_type(const char *s);
 
+// OBJ POOL STUFF
+
+void object_bucket_free(struct object_bucket *bucket);
+struct parsed_object_pool *parsed_object_pool_new(size_t initial_bucket_count);
+void parsed_object_pool_free(struct parsed_object_pool *pool);
+
+void parsed_object_pool_insert(struct parsed_object_pool *pool,
+                               struct object *obj);
+
+struct object *find_object(struct parsed_object_pool *pool,
+                           struct object_id *oid);
 #endif
--- src/object.c
+++ src/object.c
@@ -1,4 +1,8 @@
 #include "object.h"
+#include "helper.h"
+#include "sha256.h"
+#include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 const char *object_type_to_string(enum object_type type) {
@@ -15,4 +19,66 @@ enum object_type string_to_object_type(const char *s) {
   }
 
   return OBJ_UNKNOWN;
+}
+
+struct parsed_object_pool *parsed_object_pool_new(size_t initial_bucket_count) {
+  struct parsed_object_pool *pool = xmalloc(1, struct parsed_object_pool);
+
+  pool->bucket_nr = initial_bucket_count;
+  pool->buckets = xcalloc(pool->bucket_nr, struct object_bucket *);
+
+  return pool;
+}
+
+void object_bucket_free(struct object_bucket *bucket) {
+  while (bucket) {
+    free(bucket->obj);
+    struct object_bucket *temp = bucket;
+    bucket = bucket->next;
+    free(temp);
+  }
+}
+
+void parsed_object_pool_free(struct parsed_object_pool *pool) {
+  for (size_t i = 0; i < pool->bucket_nr; i++) {
+    object_bucket_free(pool->buckets[i]);
+  }
+
+  free(pool->buckets);
+  free(pool);
+}
+
+void parsed_object_pool_insert(struct parsed_object_pool *pool,
+                               struct object *obj) {
+  size_t first_bytes;
+  memcpy(&first_bytes, obj->oid.hash, sizeof(size_t));
+  size_t index = first_bytes % pool->bucket_nr;
+
+  struct object_bucket *new_bucket = xmalloc(1, struct object_bucket);
+  new_bucket->next = pool->buckets[index];
+  new_bucket->obj = obj;
+
+  pool->buckets[index] = new_bucket;
+}
+
+struct object *find_object(struct parsed_object_pool *pool,
+                           struct object_id *oid) {
+
+  size_t first_bytes;
+  memcpy(&first_bytes, oid->hash, sizeof(size_t));
+  size_t index = first_bytes % pool->bucket_nr;
+
+  if (!pool->buckets[index]) {
+    return NULL;
+  }
+
+  struct object_bucket *bucket = pool->buckets[index];
+  while (bucket) {
+    if (oideq(&bucket->obj->oid, oid))
+      return bucket->obj;
+
+    bucket = bucket->next;
+  }
+
+  return NULL;
 }

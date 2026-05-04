6017e7b021046bc10e0042b98a7aaeb4f1b84153
eitankushnir <etn121415@gmail.com>
2026-05-04 16:54:56 +0300

    convert between obj type and string rep

--- include/object.h
+++ include/object.h
@@ -2,10 +2,15 @@
 #define OBJECT_H
 
 enum object_type {
-  OBJ_NONE = 0, // Usually means some sort of failure to find or identify.
+  OBJ_UNKNOWN = 0, // Usually means some sort of failure to find or identify.
   OBJ_BLOB = 1,
   OBJ_TREE = 2,
   OBJ_COMMIT = 3,
 };
+
+char *object_type_names[] = {"unknown", "blob", "tree", "commit"};
+
+const char *object_type_to_string(enum object_type type);
+enum object_type string_to_object_type(const char *s);
 
 #endif
---
+++ src/object.c
@@ -0,0 +1,18 @@
+#include "object.h"
+#include <string.h>
+
+const char *object_type_to_string(enum object_type type) {
+  if (type < 0 || type > 3)
+    return "unknown";
+
+  return object_type_names[type];
+}
+
+enum object_type string_to_object_type(const char *s) {
+  for (int i = 0; i < 4; i++) {
+    if (strcmp(object_type_names[i], s) == 0)
+      return (enum object_type)i;
+  }
+
+  return OBJ_UNKNOWN;
+}

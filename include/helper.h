9050ce6ded180583395cc1ea3401dfc87dc56c7a
eitankushnir <etn121415@gmail.com>
2026-05-04 12:10:33 +0300

    memeory allocation wrappers and a mkdir wrapper

--- include/helper.h
+++ include/helper.h
@@ -1,6 +1,18 @@
 #ifndef HELPER_H
 #define HELPER_H
+#include <stddef.h>
 
-void die(const char *format, ...);
+#define xmalloc(n, type) (_xmalloc((n), (sizeof(type))))
+#define xcalloc(n, type) (_xcalloc((n), (sizeof(type))))
+#define xrealloc(ptr, n, type) (_xrealloc((ptr), (n), (sizeof(type))))
+
+void die(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+void *_xmalloc(size_t num, size_t size);
+void *_xcalloc(size_t num, size_t size);
+void *_xrealloc(void *ptr, size_t num, size_t size);
+
+void create_directory(const char *path);
+int create_directory_gently(const char *path);
 
 #endif
--- src/helper.c
+++ src/helper.c
@@ -1,7 +1,12 @@
 #include "helper.h"
+#include <asm-generic/errno-base.h>
+#include <errno.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+
 void die(const char *format, ...) {
   va_list vargs;
   va_start(vargs, format);
@@ -9,4 +14,52 @@ void die(const char *format, ...) {
   fprintf(stderr, "\n");
   va_end(vargs);
   exit(128);
+}
+
+void *_xmalloc(size_t num, size_t size) {
+  void *ptr = malloc(num * size);
+  if (!ptr) {
+    die("Fatal: Failed to allocate %zu bytes", num * size);
+  }
+
+  return ptr;
+}
+
+void *_xcalloc(size_t num, size_t size) {
+  void *ptr = calloc(num, size);
+  if (!ptr) {
+    die("Fatal: Failed to allocate %zu bytes", num * size);
+  }
+
+  return ptr;
+}
+
+void *_xrealloc(void *ptr, size_t num, size_t size) {
+  void *new_ptr = realloc(ptr, size * num);
+  if (!ptr) {
+    die("Fatal: Failed to reallocate %zu bytes", num * size);
+  }
+
+  return new_ptr;
+}
+
+int create_directory_gently(const char *path) {
+  if (mkdir(path, 0755) != 0) {
+    if (errno != EEXIST)
+      return -1;
+
+    // If the path exists but isnt a directory.
+    struct stat st;
+    if (stat(path, &st) != 0)
+      return -1;
+    if (!S_ISDIR(st.st_mode))
+      return -1;
+  }
+
+  return 0;
+}
+
+void create_directory(const char *path) {
+  if (create_directory_gently(path) != 0)
+    die("Fatal: Failed to create directory at: %s", path);
 }

b9bcba41a1a5aae6f12a28004d2aaf4bbe59b80f
eitankushnir <etn121415@gmail.com>
2026-05-04 11:50:35 +0300

    simple die function

---
+++ include/helper.h
@@ -0,0 +1,6 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+void die(const char *format, ...);
+
+#endif
---
+++ src/helper.c
@@ -0,0 +1,12 @@
+#include "helper.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+void die(const char *format, ...) {
+  va_list vargs;
+  va_start(vargs, format);
+  vfprintf(stderr, format, vargs);
+  fprintf(stderr, "\n");
+  va_end(vargs);
+  exit(128);
+}

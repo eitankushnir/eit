fe67c55fdc0306ebb15b421c7c811725d1405492
eitankushnir <etn121415@gmail.com>
2026-05-05 19:01:28 +0300

    automatic help option and tag generated from struct info for every command

--- src/parse-options.c
+++ src/parse-options.c
@@ -2,6 +2,7 @@
 #include "helper.h"
 #include <inttypes.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,6 +13,7 @@ enum option_form {
   SHORT_MULTIPLE,
   DASH_DASH,
   NOT_AN_OPTION,
+  HELP,
 };
 
 static enum option_form get_option_form(char *arg) {
@@ -20,6 +22,10 @@ static enum option_form get_option_form(char *arg) {
   size_t len = strlen(arg);
   if (len <= 1)
     return NOT_AN_OPTION;
+
+  if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0) {
+    return HELP;
+  }
 
   if (arg[0] == '-' && arg[1] != '-') {
     return len > 2 ? SHORT_MULTIPLE : SHORT_UNSTUCK;
@@ -43,9 +49,12 @@ static char *get_option_argument(int argc, char **argv, int argi) {
   if (form == DASH_DASH)
     return NULL;
   if (form == SHORT_UNSTUCK || form == LONG_UNSTUCK || form == SHORT_MULTIPLE) {
+    if (argi + 1 > argc || argv[argi + 1][0] == '-')
+      return NULL;
+
     char *optarg = argv[argi + 1];
     argv[argi + 1] = NULL;
-    return argi + 1 > argc ? NULL : optarg;
+    return optarg;
   }
   if (form == LONG_STUCK)
     return strchr(arg, '=') + 1;
@@ -71,7 +80,7 @@ static void handle_optarg(struct option *opt, char *optname, int argc,
 
     memcpy(opt->value, &a, sizeof(int));
   } else if (opt->type == OPT_STRING) {
-    *(char **)opt->value = strdup(optarg);
+    *(char **)opt->value = optarg;
   }
 }
 
@@ -188,8 +197,107 @@ static void parse_long(int argc, char **argv, int argi, struct option *opts) {
   argv[argi] = NULL;
 }
 
-int parse_options(int argc, char **argv, struct option *opts, char **usagestr) {
+static int get_longest_name(struct option *opts) {
+  int i = 0;
+  int max = 0;
+  while (opts[i].type != OPT_END) {
+    int len = 0;
+    if (opts[i].long_name)
+      len += strlen(opts[i].long_name);
+    if (opts[i].short_name)
+      len += 1;
 
+    if (opts[i].flags & OPT_NONEG)
+      len += 9; // all the puncuation and spaces.
+    else
+      len += 12;
+
+    if (opts[i].argh)
+      len += strlen(opts[i].argh) + 3;
+
+    max = len > max ? len : max;
+    i++;
+  }
+
+  return max;
+}
+
+static void print_option(struct option *opt, int help_offset) {
+  // Print name(s).
+  printf("    ");
+  int total_print = 4;
+  if (opt->short_name && opt->long_name) {
+    if (opt->flags & OPT_NONEG) {
+      printf("-%c, --%s", opt->short_name, opt->long_name);
+      total_print += 6 + strlen(opt->long_name);
+    } else {
+      printf("-%c, --[no-]%s", opt->short_name, opt->long_name);
+      total_print += 11 + strlen(opt->long_name);
+    }
+  } else if (opt->short_name) {
+    printf("-%c", opt->short_name);
+    total_print += 2;
+  } else if (opt->long_name) {
+    if (opt->flags & OPT_NONEG) {
+      printf("--%s", opt->long_name);
+      total_print += 2 + strlen(opt->long_name);
+    } else {
+      printf("--[no-]%s", opt->long_name);
+      total_print += 7 + strlen(opt->long_name);
+    }
+  } else {
+    // Should not happen ever. purely here to catch developer mistakes.
+    die("Option without any name??");
+  }
+
+  // Printf argument help.
+  if (opt->argh) {
+    if (strpbrk(opt->argh, "<>[]()|")) {
+      printf(" %s", opt->argh);
+      total_print += 1 + strlen(opt->argh);
+    } else if (opt->flags & OPT_OPTARG) {
+      printf(" [<%s>]", opt->argh);
+      total_print += 5 + strlen(opt->argh);
+    } else {
+      printf(" <%s>", opt->argh);
+      total_print += 3 + strlen(opt->argh);
+    }
+  }
+
+  for (int i = 0; i < help_offset - total_print; i++) {
+    printf(" ");
+  }
+
+  size_t helplen = strlen(opt->help);
+  for (size_t i = 0; i < helplen; i++) {
+    printf("%c", opt->help[i]);
+    if (opt->help[i] == '\n') {
+      for (int i = 0; i < help_offset; i++) {
+        printf(" ");
+      }
+    }
+  }
+
+  printf("\n\n");
+}
+
+static void print_help(struct option *opts, char **usagestr) {
+
+  int i = 0;
+  while (usagestr[i]) {
+    printf("%s\n", usagestr[i]);
+    i++;
+  }
+
+  i = 0;
+  int off = get_longest_name(opts) + 8;
+  while (opts[i].type != OPT_END) {
+    print_option(&opts[i], off);
+    i++;
+  }
+}
+
+int parse_options(int argc, char **argv, struct option *opts, char **usagestr) {
   for (int i = 0; i < argc; i++) {
     enum option_form form = get_option_form(argv[i]);
     if (form == NOT_AN_OPTION)
@@ -200,10 +308,14 @@ int parse_options(int argc, char **argv, struct option *opts, char **usagestr) {
     }
     if (form == SHORT_UNSTUCK)
       parse_short_unstuck(argc, argv, i, opts);
-    if (form == SHORT_MULTIPLE)
+    else if (form == SHORT_MULTIPLE)
       parse_short_multiple(argc, argv, i, opts);
-    if (form == LONG_UNSTUCK || form == LONG_STUCK)
+    else if (form == LONG_UNSTUCK || form == LONG_STUCK)
       parse_long(argc, argv, i, opts);
+    else if (form == HELP) {
+      print_help(opts, usagestr);
+      exit(0);
+    }
   }
 
   return cleanup_argv(argc, argv);
